ff2e420aa1d722e9bdb416c0a919b4b767b7ed9d
sigreer <42386868+sigreer@users.noreply.github.com>
2023-01-13 20:05:02 +0000

    first commit

---
+++ cfcontrol.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+## Cloudflare Control Script for the management of domains using Cloudflare's API
+source ./.env
+export CF_API_EMAIL=simon.greer@tilttech.co.uk
+source ./.secrets
+for domain in $(cat domains.txt); do \
+  curl -X POST -H "X-Auth-Key: $CF_API_KEY" -H "X-Auth-Email: $CF_API_EMAIL" \
+  -H "Content-Type: application/json" \
+  "https://api.cloudflare.com/client/v4/zones" \
+  --data '{"account": {"id": "'$ACCOUNT_ID'"}, "name":"'$domain'","jump_start":true}'; done
