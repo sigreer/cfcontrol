ca4daea6203d28ec29399177377ea47b7a1abaad
sigreer <42386868+sigreer@users.noreply.github.com>
2023-01-13 20:06:06 +0000

    correction

--- cfcontrol.sh
+++ cfcontrol.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ## Cloudflare Control Script for the management of domains using Cloudflare's API
 source ./.env
-export CF_API_EMAIL=simon.greer@tilttech.co.uk
 source ./.secrets
 for domain in $(cat domains.txt); do \
   curl -X POST -H "X-Auth-Key: $CF_API_KEY" -H "X-Auth-Email: $CF_API_EMAIL" \
