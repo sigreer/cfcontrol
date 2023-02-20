98994878e411be5f9994b207ff93fde024685c3c
sigreer <simon.greer@tilttech.co.uk>
2023-02-20 21:53:32 +0000

    redid file checks

--- cfcontrol.sh
+++ cfcontrol.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
+
+
 ## Cloudflare Control Script for the management of domains using Cloudflare's API
-source ./.env
-source ./.secrets
+if [[ -e ~/.env ]]; then
+    source ~/.env
+elif [[ -e "$PWD"/.env ]]; then
+    source "$PWD"/.env
+else
+    echo "Please add an environment file to ~/.env or current directory."
+    exit 0;
+fi
+if [[ -e ~/.secrets ]]; then
+    source ~/.secrets
+elif [[ -e "$PWD"/.secrets ]]; then
+    source "$PWD"/.secrets
+else
+    echo "Please add an environment file to ~/.secrets or current directory."
+    exit 0;
+fi
+echo "made it"
+
+
+## Formatting and styles
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
 
 function testFunction () {
     echo "Subdomain: $subdomain"
@@ -12,16 +42,16 @@ function testFunction () {
 }
 
 function getZone () {
-zoneinfo=$(curl -X GET "https://api.cloudflare.com/client/v4/zones?name=${sitedomain}&status=active&account.id=${ACCOUNT_ID}&page=1&per_page=100&order=status&direction=desc&match=all" -H "X-Auth-Email: $CF_API_EMAIL" -H "X-Auth-Key: $CF_API_KEY" -H "Content-Type: application/json")
+zoneinfo=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=${sitedomain}&status=active&account.id=${ACCOUNT_ID}&page=1&per_page=100&order=status&direction=desc&match=all" -H "X-Auth-Email: $CF_API_EMAIL" -H "X-Auth-Key: $CF_API_KEY" -H "Content-Type: application/json")
 zoneid=$(echo $zoneinfo | jq '.result[].id' | tr -d '"')
 }
 
 function addsite () {
-    curl -X POST -H "X-Auth-Key: $CF_API_KEY" -H "X-Auth-Email: $CF_API_EMAIL" -H "Content-Type: application/json" "https://api.cloudflare.com/client/v4/zones" --data "{\"account\":{\"id\":\"$ACCOUNT_ID\"},\"name\":\"$site\",\"jump_start\":true}"
+    curl -s -X POST -H "X-Auth-Key: $CF_API_KEY" -H "X-Auth-Email: $CF_API_EMAIL" -H "Content-Type: application/json" "https://api.cloudflare.com/client/v4/zones" --data "{\"account\":{\"id\":\"$ACCOUNT_ID\"},\"name\":\"$site\",\"jump_start\":true}"
 }
 
 function addSubDomainA () {
-  curl -X POST "https://api.cloudflare.com/client/v4/zones/${zoneid}/dns_records" -H "X-Auth-Email: $CF_API_EMAIL" -H "X-Auth-Key: $CF_API_KEY" -H "Content-Type: application/json" --data "{\"type\":\"A\",\"name\":\"$subdomain\",\"content\":\"$arecord\",\"ttl\":3600,\"priority\":10,\"proxied\":false}"
+  curl -s -X POST "https://api.cloudflare.com/client/v4/zones/${zoneid}/dns_records" -H "X-Auth-Email: $CF_API_EMAIL" -H "X-Auth-Key: $CF_API_KEY" -H "Content-Type: application/json" --data "{\"type\":\"A\",\"name\":\"$subdomain\",\"content\":\"$arecord\",\"ttl\":3600,\"priority\":10,\"proxied\":false}"
 }
 
 #echo "3"
@@ -47,23 +77,34 @@ function addSubDomainCs () {
 }
 
 function usage () {
-    echo "You need to specify parameters and arguments to use this script."
-    echo ""
-    echo "   To add a domain A record, use the -A flag followed by the fully-qualified domain and the IP"
-    echo "       example:"
-    echo "          cfcontrol -A subdomain.sitedomain.co.uk 122.222.211.212"
-    echo ""
-    echo "   To add a CNAME record, use the -C flag followed by the source and target records as FQDNs"
-    echo "       exammple:"
-    echo "          cfcontrol -C subdomain.sitedomain.co.uk target.domain.com"
-    echo ""
-    echo "   To add a new site, use the -S flag followed by the site master domain"
-    echo "       example:"
-    echo "          cfcontrol -S mynewsite.com"
-    echo ""
-    echo "   To bulk import records from a text or CSV file, add the filename after any flag"
-    echo "       example:"
-    echo "           cfcontrol -A domains.txt"
+    printf "
+ "$MAGENTA"
+                   _                 _  __ _                
+                  | |               | |/ _| |               
+               ___| | ___  _   _  __| | |_| | __ _ _ __ ___ 
+              / __| |/ _ \\| | | |/ _\` |  _| |/ _\` | \'__/ _ \\
+             | (__| | (_) | |_| | (_| | | | | (_| | | |  __/
+              \___|_|\___/ \\__,_|\\__,_|_| |_|\\__,_|_|  \\___|
+               "$RED"============================================="$RESET"
+              "$WHITE"C  O  N  T  R  O  L  "$RED"/"$BLUE" * "$RED"\\"$WHITE"  S  C  R  I  P  T"$RESET"
+                  
+                                               
+    You need to specify parameters and arguments to use this script.
+    
+    Add an A record to a pre-existing site:"$RESET"
+        "$MAGENTA"cfcontrol "$RESET""$RED"-A"$RESET""$CYAN" subdomain.sitedomain.co.uk"$RESET" "$CYAN"122.222.211.212"$RESET"
+    
+    Add a CNAME record to a pre-existing site:"$RESET"
+        "$MAGENTA"cfcontrol "$RESET""$RED"-C"$RESET""$CYAN" subdomain.sitedomain.co.uk"$RESET" "$CYAN"target.domain.com"$RESET"
+    
+    Create a new site:"$RESET"
+        "$MAGENTA"cfcontrol "$RESET""$RED"-S"$RESET""$CYAN" mynewsite.com"$RESET"
+    
+    You can bulk import any record type by using a .txt or .csv file after the parameter flag:"$RESET"
+        "$MAGENTA"cfcontrol "$RESET""$RED"-A"$RESET""$CYAN" /home/subdomains.txt"$RESET"
+
+
+"
 }
 #echo "5"
 
